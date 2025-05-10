f3d652817ebb4e4a3501176efd605352fa0e8d2a
blehbleh-test <appyfizzcan@gmail.com>
2025-05-10 22:55:54 +0530

    changes made to app.py

--- src/app.py
+++ src/app.py
@@ -360,7 +360,7 @@ if submit:
 
     # Step 9: Output the PDF report
     pdf_output = BytesIO()
-    pdf.output(pdf_output)
+    pdf.output(pdf_output, 'F')
     pdf_output.seek(0)
 
     st.download_button("Download Your Report", pdf_output, file_name="Diabetes_Risk_Report.pdf", mime="application/pdf")
